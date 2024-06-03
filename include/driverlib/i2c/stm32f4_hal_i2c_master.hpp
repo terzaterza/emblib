5a2610872260fd2288e93637df61d427e759ea37
terza <lukaterzicbg@gmail.com>
2024-06-03 18:07:25 +0200

    moved driverlib to include

--- include/driverlib/i2c/stm32f4_hal_i2c_master.hpp
+++ include/driverlib/i2c/stm32f4_hal_i2c_master.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "emblib.hpp"
-#include "drivers/i2c_address.hpp"
-#include "drivers/i2c_master.hpp"
+#include "emblib/emblib.hpp"
+#include "emblib/drivers/i2c_address.hpp"
+#include "emblib/drivers/i2c_master.hpp"
 #include "stm32f4xx_hal.h"
 
 namespace emblib::drivers {
@@ -10,14 +10,14 @@ namespace emblib::drivers {
 class stm32f4_hal_i2c_master : public i2c_master {
 
 public:
-    explicit stm32f4_hal_i2c_master(I2C_HandleTypeDef& i2c_handle)
+    explicit stm32f4_hal_i2c_master(const I2C_HandleTypeDef& i2c_handle)
         : i2c_handle(i2c_handle) {}
 
 private:
     const uint32_t TIMEOUT = 0;
     const uint32_t PROBE_TRIALS = 1;
 
-    I2C_HandleTypeDef& i2c_handle;
+    I2C_HandleTypeDef i2c_handle;
     
 
     status write_handler(const i2c_address address, const uint8_t* data, const size_t size) override
--- include/driverlib/uart/stm32f4_hal_usart.hpp
+++ include/driverlib/uart/stm32f4_hal_usart.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "emblib.hpp"
-#include "drivers/uart.hpp"
+#include "emblib/emblib.hpp"
+#include "emblib/drivers/uart.hpp"
 #include "stm32f4xx_hal.h"
 
 namespace emblib::drivers {
@@ -9,13 +9,13 @@ namespace emblib::drivers {
 class stm32f4_hal_usart : public uart {
 
 public:
-    explicit stm32f4_hal_usart(USART_HandleTypeDef& handle)
+    explicit stm32f4_hal_usart(const USART_HandleTypeDef& handle)
         : usart_handle(handle) {}
 
 private:
     const uint32_t TIMEOUT = 0;
 
-    USART_HandleTypeDef& usart_handle;
+    USART_HandleTypeDef usart_handle;
 
     status write_handler(const uint8_t* data, const size_t size)
     {
