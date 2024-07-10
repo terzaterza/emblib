1c66ac13be31d753fe0a6064309ea525cc023b34
terza <lukaterzicbg@gmail.com>
2024-07-10 18:44:28 +0200

    extended kalman filter redesign

--- include/emblib/algo/ekf.hpp
+++ include/emblib/algo/ekf.hpp
@@ -6,45 +6,31 @@
 
 namespace emblib::algo {
 
-template <size_t state_dim, size_t obs_dim>
+template <size_t state_dim, size_t obs_dim, typename ext_input_t = void>
 class ekf {
 
 public:
-using state_vec = math::vector<float, state_dim>;
-using state_mat = math::matrix<float, state_dim, state_dim>;
-using obs_vec   = math::vector<float, obs_dim>;
-using obs_mat   = math::matrix<float, obs_dim, state_dim>;
+    using state_vec = math::vector<float, state_dim>;
+    using obs_vec   = math::vector<float, obs_dim>;
 
-using state_transition = state_vec (*)(const state_vec& state);
-using obs_transition   = obs_vec (*)(const state_vec& state);
-using state_jacob = state_mat (*)(const state_vec& state);
-using obs_jacob   = obs_mat (*)(const state_vec& state);
-
-public:
     explicit ekf(
-        state_transition state_transition_func,
-        obs_transition obs_transition_func,
-        state_jacob state_jacob_func,
-        obs_jacob obs_jacob_func,
-        math::matrix<float, state_dim, state_dim> process_covariance,
-        math::matrix<float, obs_dim, obs_dim> obs_covariance
-    ) noexcept {}
+        const math::matrix<float, state_dim, state_dim>& process_covariance,
+        const math::matrix<float, obs_dim, obs_dim>& obs_covariance
+    ) noexcept : Q(process_covariance), R(obs_covariance) {}
 
     /**
      * Update the state of the filter with a new observation
-     * 
-     * @todo Add external input vector to f and update
     */
-    void update(const obs_vec& observation) noexcept
+    state_vec update(const obs_vec& observation, const ext_input_t& ext_input) noexcept
     {
-        state_mat Fk = F(state);
+        state_jacob Fk = F(state, ext_input);
 
-        state_vec state_predict = f(state);
-        state_mat cov_predict = Fk * cov * math::transpose(Fk) + Q;
+        state_vec state_predict = f(state, ext_input);
+        state_jacob cov_predict = Fk * cov * math::transpose(Fk) + Q;
 
         obs_vec obs_diff = observation - h(state_predict);
-        obs_mat Hk = H(state_predict);
-        obs_mat HkT = math::transpose(Hk);
+        obs_jacob Hk = H(state_predict);
+        obs_jacob HkT = math::transpose(Hk);
 
         auto Pk_HkT = cov_predict * HkT;
         auto residual_cov = Hk * Pk_HkT + R;
@@ -52,6 +38,8 @@ public:
 
         state = state_predict + kalman_gain * obs_diff;
         cov = (math::identity<float, state_dim>() - kalman_gain * Hk) * cov_predict;
+
+        return state;
     }
 
     /**
@@ -59,19 +47,50 @@ public:
      * 
      * @returns Copy of the state vector
     */
-    state_vec get_state() noexcept
+    state_vec get_state() const noexcept
     {
         return state;
     }
 
-private:
-    state_vec state;
-    state_mat cov;
+protected:
+    using state_jacob = math::matrix<float, state_dim, state_dim>;
+    using obs_jacob   = math::matrix<float, obs_dim, state_dim>;
 
-    const state_transition f;
-    const obs_transition h;
-    const state_jacob F;
-    const obs_jacob H;
+private:
+    /**
+     * State transition function
+     * 
+     * Compute the state at the next time step based on current state and external input
+     */
+    virtual state_vec f(const state_vec& state, const ext_input_t& ext_input) const noexcept = 0;
+
+    /**
+     * State Jacobian
+     * 
+     * Represents matrix of derivatives of each component of function `f`'s output
+     * with respect to each component of the state vector
+     */
+    virtual state_jacob F(const state_vec& state, const ext_input_t& ext_input) const noexcept = 0;
+
+    /**
+     * Observation mapping function
+     * 
+     * Map the predicted state to the expected observation vector
+     */
+    virtual obs_vec h(const state_vec& state) const noexcept = 0;
+
+    /**
+     * Observation Jacobian
+     * 
+     * Matrix of derivatives of components of function `h`'s output
+     * with respect to each component of the state vector
+     */
+    virtual obs_jacob H(const state_vec& state) const noexcept = 0;
+
+
+    state_vec state;
+    math::matrix<float, state_dim, state_dim> cov;
+
     const math::matrix<float, state_dim, state_dim> Q;
     const math::matrix<float, obs_dim, obs_dim> R;
 
