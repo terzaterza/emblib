1b8ad282bd4c8bd90e8bb8096a49e421ddc5c67a
terza <lukaterzicbg@gmail.com>
2024-10-27 22:20:55 +0100

    pin rework

---
+++ include/emblib/drivers/gpio/gpio_pin.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "emblib/emblib.hpp"
+
+namespace emblib::drivers {
+
+class gpio_pin {
+
+public:
+    explicit gpio_pin() = default;
+    virtual ~gpio_pin() = default;
+
+    /* Copy operations not allowed */
+    gpio_pin(const gpio_pin&) = delete;
+    gpio_pin& operator=(const gpio_pin&) = delete;
+
+    /* Move operations not allowed */
+    gpio_pin(gpio_pin&&) = delete;
+    gpio_pin& operator=(gpio_pin&&) = delete;
+
+    /** @todo Can change to enum if needed */
+    typedef bool pin_state_t;
+
+    /**
+     * Read the current state of the pin into the user provided buffer
+     * @returns `true` if the `state` value now has valid data
+     */
+    virtual bool read(pin_state_t& state) noexcept = 0;
+
+    /**
+     * Write the given state to the pin
+     * @returns `true` for a successful write
+     */
+    virtual bool write(pin_state_t state) noexcept = 0;
+    
+    /**
+     * Toggle the current state
+     * @returns `true` if toggled successfully
+     */
+    virtual bool toggle() noexcept = 0;
+
+    /** @todo Add set_callback, set_interrupt_mode, set_pull_mode */
+
+};
+
+}
